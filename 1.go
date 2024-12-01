0532e6b38e01b91900e00f7be17d775f6d665b97
Abuzeid <abuzeid1999@gmail.com>
2024-12-01 10:32:18 +0200

    challenge 2

--- 1.go
+++ 1.go
@@ -16,13 +16,12 @@ func main() {
 
 	}
 	defer file.Close()
-	var inputArr1 = make([]int, 1000, 1000)
-	var inputArr2 = make([]int, 1000, 1000)
+	var inputArr1 = make([]int, 0, 1000)
+	var inputArr2 = make([]int, 0, 1000)
 	reader := csv.NewReader(file)
 
 	recordes, err := reader.ReadAll()
 	for _, record := range recordes {
-
 		value, _ := strconv.Atoi(record[0])
 		inputArr1 = append(inputArr1, value)
 		secondValue, _ := strconv.Atoi(record[1])
@@ -39,7 +38,38 @@ func main() {
 		} else if number-inputArr2[index] < 0 {
 			DifferenceSum += (number - inputArr2[index]) * -1
 		}
-
 	}
 	fmt.Println(DifferenceSum)
+	// Second Challenge Find Similarities	occurrences := make(map[int]int)
+
+	occurrences := make(map[int]int)
+	binarysearch := func(number int) bool {
+		high := 999
+		low := 0
+
+		for low <= high {
+			mid := low + (high-low)/2
+
+			if inputArr1[mid] == number {
+				occurrences[number] += 1
+
+				return true
+			} else if inputArr1[mid] > number {
+				high = mid - 1
+			} else if inputArr1[mid] < number {
+				low = mid + 1
+			}
+
+		}
+		return false
+	}
+
+	for _, num := range inputArr2 {
+		binarysearch(num)
+	}
+	var sum int
+	for i, num := range occurrences {
+		sum += i * num
+	}
+	fmt.Println(sum)
 }
